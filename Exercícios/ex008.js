fe318ebe4ed6af2b63a750ed632b219839713198
henriquelz <zielkehenrique@gmail.com>
2022-05-18 14:44:33 -0300

    Initial commit

---
+++ Exercícios/ex008.js
@@ -0,0 +1,7 @@
+console.log('O Js é a minha area!')
+var vel = 61
+console.log(`a Velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){ // condição simples //
+    console.log(`você ultrapassou o limite de velocidade, MULTADO!`)
+}
+console.log(`dirija sempre com sinto de segurança`)
